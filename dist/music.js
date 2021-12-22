e570eea3cf20a1becd3d7b27d30bf28372d9b217
znkgitub <2930119331@qq.com>
2021-12-22 13:19:06 +0800

    Site updated: 2021-12-22 13:19:06

---
+++ dist/music.js
@@ -0,0 +1,15 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    lrcType: 3,
+    audio: [
+      {
+        name: "起风了",
+        artist: '张宁珂',
+        url: 'https://1driv-my.sharepoint.com/personal/stor_1driv_onmicrosoft_com/Documents/mp3/2021/12/80000 - 起风了-（间奏加长版）.mp3',
+        cover: 'https://s2.loli.net/2021/12/19/RpaLlisnU3Su2O7.png',
+        lrc: 'song-post/qifengle.lrc'
+      },
+    ]
+});
